31bc39fe94a68701846c03ef19e554d871c9035a
ahhkiivci <dexxnattionn@gmail.com>
2025-09-24 15:41:50 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ AkhirahHenry_assignment_3.py
@@ -0,0 +1,13 @@
+student_name = "Akhirah"
+current_gpa = 3.5
+study_hours = 20
+social_points = 47
+stress_level = 89
+
+print(f"Welcome, {student_name}!")
+print("Your current stats are:")
+print(f"Current GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
+
